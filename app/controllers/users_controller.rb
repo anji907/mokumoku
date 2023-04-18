d1881d198724fdc45c18ce38a422c857d4a96509
anji907 <anji.sk129@gmail.com>
2023-04-18 12:10:44 +0900

    Fix: ユーザーオブジェクトを取得する処理をまとめた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
   def new
     @user = User.new
   end
@@ -17,6 +19,11 @@ class UsersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 
